3608ba53621467d0a4fd25e10f6daf8434157bca
Andrey Nesterov <polyshinnel@gmail.com>
2022-11-30 01:02:52 +0300

    add gallery and readme

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Разработана для сайта nebesa.online на движке october.cms версии 1.1
 
 Сверстаны и подключены все основные страницы
+
+
+## Установка October
+
+### Скачиваем October CMS
+composer create-project october/october:1.1.* .
+
+### Разворачиваем его на сервере
+php artisan october:install
--- assets/scripts/owl-config.js
+++ assets/scripts/owl-config.js
@@ -14,3 +14,22 @@ $('.banner__arrow_right').on('click', function() {
 $('.banner__arrow_left').on('click', function() {
     $(this).parent().find('.owl-prev').trigger('click');
 });
+
+
+$('.gallery-block').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    items: 4,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        570:{
+            items:4,
+            margin: 25,
+            nav:false
+        },
+    }
+})
